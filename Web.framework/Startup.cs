5560109aed2012701badbda4c8bc0166a674a3f4
Vladimir Rõkovanov <vladimir.rokovanov@itcollege.ee>
2016-08-13 22:48:29 +0300

    Add project files.

---
+++ Web.framework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.framework.Startup))]
+namespace Web.framework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
